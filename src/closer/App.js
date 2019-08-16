87e54ba3c1ca0a8f8876a4bc4772a9bc57083f1d
Emanuel Rouse <emanuelrouse@users.noreply.github.com>
2019-08-15 21:26:40 -0400

    Add Landing page component

--- src/closer/App.js
+++ src/closer/App.js
@@ -1,46 +1,12 @@
 import React from 'react';
-import Nav from './components/Nav';
+import { Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
 import './App.css';
 
 function App() {
   return (
-    <main className='App'>
-      <Nav />
-      <header role="banner">
-        <h1>Closer App</h1>
-        <h2>Close the distance</h2>
-      </header>
-      
-      <section>
-        <header>
-          <h3>It's time to close the distance</h3>
-        </header>
-        <p>Closer helps you work out the steps required to close the distance and be with your loved one once and for all.</p>
-      </section>
-      <section>
-        <header>
-          <h3>Create your action plan today</h3>
-        </header>
-        <form action="" className="signup-form">
-          <div>
-            <label htmlFor="first-name">First name</label>
-            <input placeholder="first-name" type="text" name="first-name" id="first-name" />
-          </div>
-          <div>
-            <label htmlFor="last-name">Last name</label>
-            <input placeholder="last-name" type="text" name="last-name" id="last-name" />
-          </div>
-          <div>
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" id="username" />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
-          </div>
-          <button type="submit">Sign up</button>
-        </form>
-      </section>
+    <main className="app">
+      <Route path='/' component={LandingPage} />
     </main>
   );
 }
