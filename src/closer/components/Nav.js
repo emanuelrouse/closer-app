c687d5d8a11fcafcc55a9190b5d8a7311027e786
Emanuel Rouse <emanuelrouse@users.noreply.github.com>
2019-08-17 18:06:00 -0400

    Add updated Nav component

--- src/closer/components/Nav.js
+++ src/closer/components/Nav.js
@@ -5,15 +5,23 @@ import './Nav.css';
 function Nav(props) {
   return ( 
   <nav role="navigation" className={props.className}>
-    <Link to="/">
-      Home
-    </Link >
-    <Link to="/register">
-      Register
-    </Link >
-    <a href="/action-plan">
-      Action
-    </a>
+    <ul className="nav-bar">
+      <li>
+        <Link to="/home" className="nav-links">
+          Home
+        </Link >
+      </li>
+      <li>    
+        <Link to="/registration" className="nav-links">
+          Register
+        </Link >
+      </li>
+      <li>
+        <Link to="/action-plan" className="nav-links">
+          Action Plan
+        </Link>
+      </li>
+    </ul>
   </nav>
   );
 };
